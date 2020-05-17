d290e09068a579567d0818f84ca0c046d34614f2
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-17 02:39:08 -0300

    first commit

---
+++ src/pages/Mensal/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Container} from './styles';
+
+const Mensal: React.FC = () => {
+  return <Container />;
+};
+
+export default Mensal;
---
+++ src/pages/Semanal/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Semanal: React.FC = () => {
+  return <Container />;
+};
+
+export default Semanal;
