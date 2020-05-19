e35f2a1da5ee11148eab70acc1616c0283058bb2
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-19 13:32:16 -0300

    changes in page Semanal - adding carousel

--- src/pages/Semanal/index.tsx
+++ src/pages/Semanal/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Dimensions, TouchableWithoutFeedback } from 'react-native';
 
+import Carousel from 'react-native-snap-carousel';
+
+// import {
+//   TouchableWithoutFeedback,
+//   TouchableOpacity,
+// } from 'react-native-gesture-handler';
 import logoImg from '../../assets/Logo.png';
 
 import {
@@ -8,40 +14,99 @@ import {
   Header,
   TitleContainer,
   TitleContent,
+  CardList,
+  SafeAreaViewSlider,
   Card,
   CardImage,
 } from './styles';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+    uri:
+      'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+    uri:
+      'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+    uri:
+      'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
+  },
+];
+
+const sliderWidth = Dimensions.get('window').width;
+const itemWidth = Math.round(sliderWidth * 0.55);
+
+interface Promotion {
+  id: string;
+  title: string;
+  uri: string;
+}
+
 const Semanal: React.FC = () => {
+  const [activeIndexFirst, setActiveIndexFirst] = useState(0);
+  const [activeIndexSecond, setActiveIndexSecond] = useState(0);
+
+  const handlePressSecond = () => {
+    console.log(activeIndexSecond);
+  };
+
   return (
     <Container>
       <Header>
         <Image source={logoImg} resizeMode="contain" />
       </Header>
-
       <TitleContainer>Promoções da semana</TitleContainer>
       <TitleContent>Segunda e Terça</TitleContent>
-
-      <Card>
-        <CardImage
-          source={{
-            uri:
-              'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
-          }}
-          resizeMode="contain"
+      <SafeAreaViewSlider>
+        <Carousel
+          data={DATA}
+          useScrollView
+          sliderWidth={sliderWidth}
+          itemWidth={itemWidth}
+          onSnapToItem={(index) => setActiveIndexFirst(index)}
+          renderItem={({ item, index }) => (
+            <Card>
+              <CardImage
+                source={{
+                  uri: `${item.uri}`,
+                }}
+                resizeMode="cover"
+              />
+            </Card>
+          )}
         />
-      </Card>
+      </SafeAreaViewSlider>
 
       <TitleContent>Quarta e Quinta</TitleContent>
-      <Card>
-        <CardImage
-          source={{
-            uri:
-              'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
-          }}
-          resizeMode="contain"
+      <SafeAreaViewSlider>
+        <Carousel
+          data={DATA}
+          layout="default"
+          sliderWidth={sliderWidth}
+          itemWidth={itemWidth}
+          onSnapToItem={(index) => setActiveIndexSecond(index)}
+          renderItem={({ item, index }) => (
+            <Card>
+              <TouchableWithoutFeedback onPress={handlePressSecond}>
+                <CardImage
+                  source={{
+                    uri: `${item.uri}`,
+                  }}
+                  resizeMode="cover"
+                />
+              </TouchableWithoutFeedback>
+            </Card>
+          )}
         />
-      </Card>
+      </SafeAreaViewSlider>
     </Container>
   );
 };
--- src/pages/Semanal/styles.ts
+++ src/pages/Semanal/styles.ts
@@ -1,12 +1,22 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+
+interface Promotion {
+  id: string;
+  title: string;
+  uri: string;
+}
 
 export const Container = styled.View`
   flex: 1;
-  padding: 0 14px;
+  /* padding: 0 14px; */
 `;
 
 export const Header = styled.View`
   height: 60px;
+
+  padding-left: 14px;
+  padding-right: 14px;
 
   justify-content: center;
   align-items: flex-start;
@@ -14,20 +24,49 @@ export const Header = styled.View`
 
 export const TitleContainer = styled.Text`
   font-family: 'Roboto-Medium';
+  letter-spacing: 0.5px;
   font-size: 14px;
   color: #878787;
+
+  padding-left: 14px;
+  padding-right: 14px;
+  margin-top: 5px;
+  margin-bottom: 10px;
 `;
 
 export const TitleContent = styled.Text`
   font-size: 24px;
+  letter-spacing: 0.5px;
   font-family: 'Roboto-Bold';
   color: #333333;
+
+  padding-left: 14px;
+  padding-right: 14px;
+  margin-bottom: 14px;
+`;
+
+export const CardList = styled(FlatList as new () => FlatList<Promotion>).attrs(
+  {
+    numColumns: 1,
+  },
+)`
+  margin-bottom: 14px;
+`;
+
+export const SafeAreaViewSlider = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 export const Card = styled.View`
-  margin: 0;
-  padding: 0;
-  background-color: #666;
+  position: relative;
+  left: -75px;
+  /* transform: translateX(50); */
+  /* width: 190px; */
+
+  margin-left: 0;
+  padding-left: 0;
 `;
 
 export const CardImage = styled.Image`
@@ -35,6 +74,5 @@ export const CardImage = styled.Image`
   height: 224px;
   border-radius: 20px;
 
-  margin: 0;
-  padding: 0;
+  margin-left: 0;
 `;
