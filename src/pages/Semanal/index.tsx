18d7463c6e9086fa4d164b4b2b236f19ebc8f838
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-19 17:49:43 -0300

    changes in page Semanal - adding modal

--- src/pages/Semanal/index.tsx
+++ src/pages/Semanal/index.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { Image, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import {
+  Image,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Modal,
+  TouchableOpacity,
+} from 'react-native';
 
 import Carousel from 'react-native-snap-carousel';
 
-// import {
-//   TouchableWithoutFeedback,
-//   TouchableOpacity,
-// } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import logoImg from '../../assets/Logo.png';
 
 import {
@@ -14,24 +18,30 @@ import {
   Header,
   TitleContainer,
   TitleContent,
-  CardList,
   SafeAreaViewSlider,
   Card,
   CardImage,
+  ModalContainer,
+  ModalButtonClose,
+  ModalImage,
 } from './styles';
 
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     title: 'First Item',
-    uri:
-      'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
+    uri: 'https://i.gyazo.com/01b4887cc36788b03a3f07a6050fc458.jpg',
+  },
+  {
+    id: 'bd7asdbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+    uri: 'https://i.gyazo.com/3586907860fd4703a42003db216efc0b.jpg',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
     title: 'Second Item',
     uri:
-      'https://www.ibahia.com/fileadmin/user_upload/ibahia/2019/outubro/25/banana.jpg?width=1200&enable=upscale',
+      'https://www.kitano.com.br/wp-content/uploads/2019/07/SSP_2803-Brasa-maminha-com-pa%E2%95%A0%C3%BCprica-ore%E2%95%A0%C3%BCgano-e-sal.jpg',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
@@ -42,7 +52,7 @@ const DATA = [
 ];
 
 const sliderWidth = Dimensions.get('window').width;
-const itemWidth = Math.round(sliderWidth * 0.55);
+const itemWidth = Math.round(sliderWidth * 0.6);
 
 interface Promotion {
   id: string;
@@ -54,12 +64,54 @@ const Semanal: React.FC = () => {
   const [activeIndexFirst, setActiveIndexFirst] = useState(0);
   const [activeIndexSecond, setActiveIndexSecond] = useState(0);
 
+  const [modalVisible, setModalVisible] = useState(false);
+
   const handlePressSecond = () => {
     console.log(activeIndexSecond);
+    setModalVisible(!modalVisible);
   };
 
   return (
     <Container>
+      <Modal
+        animationType="slide"
+        transparent
+        visible={modalVisible}
+        onRequestClose={() => {
+          console.log('modal fechou');
+        }}
+      >
+        <ModalContainer
+          style={{
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.32,
+            shadowRadius: 5.46,
+
+            elevation: 9,
+          }}
+        >
+          {/* <TitleContent>Promoção</TitleContent> */}
+          {/* <ModalHeader>
+            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+              <Icon name="close-circle" size={24} color="#e83f5b" />
+            </TouchableOpacity>
+          </ModalHeader> */}
+          <ModalButtonClose onPress={() => setModalVisible(!modalVisible)}>
+            <Icon name="close-circle" size={24} color="#e83f5b" />
+          </ModalButtonClose>
+          <ModalImage
+            source={{
+              uri: DATA[activeIndexSecond].uri,
+            }}
+            resizeMode="contain"
+          />
+        </ModalContainer>
+      </Modal>
+
       <Header>
         <Image source={logoImg} resizeMode="contain" />
       </Header>
--- src/pages/Semanal/styles.ts
+++ src/pages/Semanal/styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components/native';
-import { FlatList } from 'react-native';
 
-interface Promotion {
-  id: string;
-  title: string;
-  uri: string;
-}
+// interface Promotion {
+//   id: string;
+//   title: string;
+//   uri: string;
+// }
 
 export const Container = styled.View`
   flex: 1;
@@ -45,14 +44,6 @@ export const TitleContent = styled.Text`
   margin-bottom: 14px;
 `;
 
-export const CardList = styled(FlatList as new () => FlatList<Promotion>).attrs(
-  {
-    numColumns: 1,
-  },
-)`
-  margin-bottom: 14px;
-`;
-
 export const SafeAreaViewSlider = styled.View`
   flex: 1;
   flex-direction: row;
@@ -61,18 +52,52 @@ export const SafeAreaViewSlider = styled.View`
 
 export const Card = styled.View`
   position: relative;
-  left: -75px;
-  /* transform: translateX(50); */
-  /* width: 190px; */
-
-  margin-left: 0;
-  padding-left: 0;
+  left: -64px;
 `;
 
 export const CardImage = styled.Image`
-  width: 190px;
+  width: 220px;
   height: 224px;
-  border-radius: 20px;
+  border-radius: 10px;
+
+  /* transform: scale(2); */
 
   margin-left: 0;
+`;
+
+export const ModalContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  /* margin: 150px 10px;
+  padding: 5px;
+  border-radius: 20px;
+
+  background-color: #ebebeb; */
+`;
+
+export const ModalHeader = styled.View`
+  width: 100%;
+  padding: 10px 5px 0 0;
+
+  align-items: flex-end;
+`;
+
+export const ModalButtonClose = styled.TouchableOpacity`
+  position: relative;
+  top: 190px;
+  right: 16px;
+  width: 100%;
+  z-index: 1;
+
+  align-items: flex-end;
+`;
+
+export const ModalImage = styled.Image`
+  width: 98%;
+  height: 95%;
+  /* background-color: pink; */
+
+  border-radius: 5px;
 `;
